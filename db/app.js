f4e22ab14473316daf38382f87e245f65a9325b5
RodrigoG24 <rodrigogalante24@hotmail.com>
2024-10-27 19:25:42 -0300

    Fix para deployar en host

--- db/app.js
+++ db/app.js
@@ -1,24 +1,47 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const mongoose = require('./db');
 const productosRoutes = require('./routes/productos');
-const loginRoutes = require('./routes/login'); // Importa la ruta de inicio de sesión
+const loginRoutes = require('./routes/login');
 const cors = require('cors');
+const helmet = require('helmet'); // Importamos helmet para manejar Content Security Policy (REQUERIDO POR Render)
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// Middleware de seguridad
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                fontSrc: ["'self'", 'https://fonts.gstatic.com', 'data:'], // URL para fuentes de Google Fonts
+                styleSrc: ["'self'", 'https://fonts.googleapis.com', "'unsafe-inline'"], // URL para estilos de Google Fonts
+            },
+        },
+    })
+);
 
+app.use(cors());
 app.use(bodyParser.json());
-console.log('Solicitud a /api/productos:');
+
+// Configuración para servir archivos estáticos desde la raíz del proyecto
+app.use(express.static(path.join(__dirname, '..'))); // Sirve archivos de la carpeta base del proyecto
+
+// Rutas para las APIs
 app.use('/api/productos', productosRoutes);
 app.use('/api/login', loginRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+// Ruta raíz para servir el archivo HTML principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
+// Inicia el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
